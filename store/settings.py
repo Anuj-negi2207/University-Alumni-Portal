efe2cbb06eed851b8193b135665defe5fe373a35
Anuj-negi2207 <pienegi22@gmail.com>
2022-11-29 07:25:00 +0530

    Added TEMPLATES:DIRS

--- store/settings.py
+++ store/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +60,7 @@ ROOT_URLCONF = 'store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
