16cb45234e87b27f8184681dcf714b0270a7709a
Anuj Singh Negi <66243709+Anuj-negi2207@users.noreply.github.com>
2022-11-30 00:39:48 +0530

    Password removed

--- store/info.py
+++ store/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.vitbhopal.ac.in'
 EMAIL_HOST_USER = 'anuj.singh2019@vitbhopal.ac.in'
-EMAIL_HOST_PASSWORD = '5806066@Anuj'
+EMAIL_HOST_PASSWORD = '********'
 EMAIL_PORT = 587
